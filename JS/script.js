1fd4819f5c8f69f16b237d1878b90da5bb336b74
AndresiniGiovanni <gio.andresini@hotmail.it>
2022-10-10 17:37:57 +0200

    style

--- JS/script.js
+++ JS/script.js
@@ -6,23 +6,36 @@
 
 function fizzBuzz() {
   let numbers = document.getElementById("numbers");
+
   for (i = 1; i <= 100; i++) {
     let div = document.createElement("div");
+
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("FizzBuzz");
-      div.innerHTML = i + " fizzBuzz";
+
+      div.className = "fizzBuzz";
+      div.innerHTML = i + " FizzBuzz";
+
       numbers.append(div);
     } else if (i % 5 === 0) {
       console.log("buzz");
+
+      div.className = "buzz";
       div.innerHTML = i + " Buzz";
+
       numbers.append(div);
     } else if (i % 3 === 0) {
       console.log("Fizz");
-      div.innerHTML = i + " fizz";
+
+      div.className = "fizz";
+      div.innerHTML = i + " Fizz";
+
       numbers.append(div);
     } else {
       console.log(i);
+
       div.innerHTML = i;
+
       numbers.append(div);
     }
   }
